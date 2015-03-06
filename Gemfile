707cc053a03a2c43012117c0b5eb8ee14b9807c7
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-03-05 17:18:17 -0800

    Replaces *.erb with *.slim; adds scraper into index page

--- Gemfile
+++ Gemfile
@@ -7,35 +7,45 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'awesome_print'
+gem 'rubygems'
+gem 'nokogiri' 
+gem 'open-uri'
+
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
 end
+
 gem 'foundation-rails'
 gem 'high_voltage'
 gem 'simple_form'
 gem 'slim-rails'
 gem 'unicorn'
 gem 'unicorn-rails'
+
 group :development do
   gem 'better_errors'
-  gem 'binding_of_caller', :platforms=>[:mri_21]
-  gem 'hub', :require=>nil
+  gem 'binding_of_caller', :platforms => [:mri_21]
+  gem 'hub', :require => nil
   gem 'quiet_assets'
   gem 'rails_layout'
   gem 'spring-commands-rspec'
 end
+
 group :development, :test do
   gem 'factory_girl_rails'
   gem 'faker'
   gem 'rspec-rails'
   gem 'sqlite3'
 end
+
 group :production do
   gem 'pg'
   gem 'rails_12factor'
 end
+
 group :test do
   gem 'capybara'
   gem 'database_cleaner'
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,57 @@
 class VisitorsController < ApplicationController
 
 	def index
-		puts 'hi!'
-		@test = 'laskdfjalskdjflaskdjfladskjf'
+		url = 'http://static.crunchbase.com/daily/content_web.html'
+		page = Nokogiri::HTML(open(url))
+
+		rounds_table = page.css('table:contains("Featured Funding Rounds") table table tbody')[0].css('table tbody')
+		companies, investors = {} , {}
+		for company_table in rounds_table
+			investors_hash = parse_investors(company_table)
+			company = parse_company(company_table, investors_hash)
+
+			investors = investors.merge(investors_hash)
+			companies = companies.merge(company) if company != nil
+		end
+		companies = companies.select { |c| c != nil }
+
+		@companies = companies
+		@investors = investors
 	end
 
 end
+
+
+	private 
+		def cb_permalink(a)
+			a['href'].scan(/[\w-]+$/)[0] if a != nil
+		end
+
+
+		def parse_investors(company_table)
+			investors = {}
+
+			invest_table = company_table.css('table tbody td:contains("Investors")')
+			for i in invest_table.css('a')
+				# Ignore any rows that are "+ x more investors".
+				if not i.text =~ /\+ \d more investors/
+					investors[cb_permalink(i)] = { name: i.text }
+				end
+			end
+			return investors
+		end
+
+
+		def parse_company(company_table, investors)
+			h2 = company_table.search('h2')
+			return nil if h2.text == ''  # Skip if company_table contains no name
+			
+			company = { 
+				name: h2.text, 
+				investors: investors.keys
+			}
+
+			permalink = cb_permalink(h2.css('a[href]')[0])
+			return { "#{permalink}" => company }
+		end
+
